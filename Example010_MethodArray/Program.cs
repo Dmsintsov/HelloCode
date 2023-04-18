f5b0d7d6c05ccdb8ca45c47dd7bd97f1df5d7ec1
Dmitry Sintsov <dmsincov@yandex.ru>
2023-04-18 20:04:43 +0200

    example 012

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,15 +1,16 @@
 ﻿int[] array = { 23, 12, 14, 16, 98, 124, 124, 235, 63 };
 
-int n=array.Length;
-int find=124;
+int n = array.Length;
+int find = 124;
+int index = 0;
 
-int index =0;
-
-while (index<n)
+while (index < n)
 {
-    if (array[index]==find)
-    {Console.WriteLine(index);
-       break; 
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
     }
     index++;
 }
+
